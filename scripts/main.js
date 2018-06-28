fa3077d18d4563a27597a8fea758e375a3d7d648
John Kennedy <nextechcatapult@arms84.localdomain>
2018-06-28 09:41:27 -0400

    function

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,7 @@ document.write(add(2,4));
 function multiply(x,y){
     let sum = 0;
     for (let i = 0 ; i< y;i++){
-        add(x,sum);
-        sum = sum + x;
+        sum =add(x,sum);
     }
     return sum;
 }
@@ -16,30 +15,23 @@ document.write(multiply(6,8));
 function power(x,y){
     let sum = 1;
     for(let i =0; i< y;i++){
-        add(x,sum);
-        sum = sum * x;
+        sum = multiply(x,sum);
  }
 return sum;
 }
 document.write("<br/>");
 document.write(power(2,8));
-function Factorial(x,y){
-    let sum = 1
-    for(let i=0;i<y;i++){
-        add(x,sum);
-        sum = x*(x-1)*(x-2)*1;
+function Factorial(y){
+    let sum=1;
+    for(let i=1;i<=y;i++){
+        sum=multiply(i,sum);
     }
     return sum;
 }
 document.write("<br/>");
 document.write(Factorial(4,4));
 function fibonacci(y){
-    var first=0,next=1,second=1;
-    for(var i=2;i<=y;i++){
-        next=first+second;
-        second=first;
-        first=next;
-    }
+   
     return next;
 }
 document.write("<br/>");
